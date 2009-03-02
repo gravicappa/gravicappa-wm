fc738310a4f9a7e90df76abbe8ef734401d27a4d
Ramil Farkhshatov <noble_yoda@mail.ru>
2009-03-02 15:40:50 +0300

    Initializing screen root window.

--- screen.scm
+++ screen.scm
@@ -1,32 +1,36 @@
 
 (define-structure screen
-  id
-  x
-  y
-  w
-  h
-  root
-  clients)
+  id x y w h root clients)
 
 (define *screens* '())
 
-(define (init-screens display)
-  (let ((num (x-screen-count display)))
-    (let loop ((i 0))
-      (cond ((< i num)
-             (init-single-screen display i)
-             (loop (+ 1 i)))))))
+(define +root-event-mask+ (bitwise-ior substructure-redirect-mask
+																			 substructure-notify-mask
+																			 button-press-mask
+																			 enter-window-mask
+																			 leave-window-mask
+																			 structure-notify-mask))
 
 (define (init-single-screen display i)
   (let* ((root (x-root-window display i))
-         ((screen (make-screen i
+         (screen (make-screen i
                                0
                                0
                                (x-display-width display i)
                                (x-display-height display i)
                                root
-                               '()))))
-    ()
-    (set *screens* (cons screen *screens*))))
+                               '())))
+    (x-change-window-attributes display root event-mask: +root-event-mask+)
+		(x-select-input display root +root-event-mask+)
+		screen))
+
+(define (init-screens display)
+	(let ((num (x-screen-count display)))
+		(set!  *screens*
+					(let loop ((i 0)
+										 (screens '()))
+						(if (< i num)
+								(loop (+ 1 i) (cons (init-single-screen display i) screens))
+								(reverse screens))))))
 
 (add-to-list *internal-startup-hook* (lambda (d) (init-screens d)))
