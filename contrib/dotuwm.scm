224d83fd389998a20a1f04a36b84209cdd5ee9ac
Ramil Farkhshatov <noble_yoda@mail.ru>
2009-03-22 03:01:30 +0300

    Updated sample configuration file.

--- contrib/dotuwm.scm
+++ contrib/dotuwm.scm
@@ -15,17 +15,18 @@
 (define (restart-bars)
   (run-command "killall dzen2")
   (set! left-bar (bar 'left))
-  (set! right-bar 
-    (run-command 
+  (set! right-bar
+    (run-command
       "status | dzen2 -ta r -tw 500 -x 1180 -fn -*-fixed-medium-r-*-*-14-*-*-*-*-*-iso10646-1")))
 
 (define (update-tag-status)
   (let ((tags (collect-all-tags)))
     (if left-bar
-        (thread-send 
+        (thread-send
           left-bar
           (with-output-to-string '()
             (lambda ()
+              (display (list *current-view* " |"))
               (for-each (lambda (t)
                           (display " ")
                           (if (string=? t *current-view*)
@@ -41,11 +42,47 @@
   (if fn
       (thread-send (current-thread) fn)))
 
+(define (make-splitter sep)
+  (lambda (str)
+    (call-with-input-string
+      str
+      (lambda (p)
+        (read-all p (lambda (p) (read-line p sep)))))))
+
+(define (make-splitter sep)
+  (lambda (str)
+    (call-with-input-string
+      str
+      (lambda (p)
+        (read-all p (lambda (p) (read-line p sep)))))))
+
+(define parse-tags
+  (let ((split (make-splitter #\,)))
+    (lambda (str)
+      (let ((tags (split str)))
+        (let loop ((tags tags)
+                   (+tags '())
+                   (-tags '()))
+          (if (pair? tags)
+              (if (char=? #\- (string-ref (car tags) 0))
+                  (loop (cdr tags)
+                        +tags
+                        (cons (substring (car tags)
+                                         1
+                                         (string-length (car tags)))
+                              -tags))
+                  (loop (cdr tags) (cons (car tags) +tags) -tags))
+              (values +tags -tags)))))))
+
 (define (tag c)
   (if c
-      (let ((tag (dmenu "Tag client:" (lambda () (collect-all-tags)))))
-        (if tag
-            (to-run (lambda () (tag-client c (list tag))))))))
+      (call-with-values
+        (lambda ()
+          (parse-tags (dmenu "Tag client:" (lambda () (collect-all-tags)))))
+        (lambda (+tags -tags)
+          (to-run (lambda ()
+                    (tag-client c +tags)
+                    (untag-client c -tags)))))))
 
 (define *prev-view* "")
 
@@ -61,7 +98,7 @@
 
 (define (toggle-fullscreen)
   (if *selected*
-      (cond 
+      (cond
         ((string=? *current-view* ".")
          (untag-client *selected* ".")
          (view-tag *prev-view*))
@@ -78,7 +115,7 @@
           (eval (with-input-from-string str (lambda () (read))))))))
 
 (define-key *top-map* (kbd "s-x s-c") (lambda () (exit)))
-(define-key *top-map* (kbd "s-x s-l") 
+(define-key *top-map* (kbd "s-x s-l")
             (lambda ()
               (run-command "killall dzen2")
               (load *user-config-file*)))
@@ -100,25 +137,27 @@
 (define-key *top-map* (kbd "s-r") (lambda () (view-tag *prev-view*)))
 
 (define-key *top-map* (kbd "s-e")
-            (lambda () (to-run (lambda () 
-                                 (eval-from-string 
+            (lambda () (to-run (lambda ()
+                                 (eval-from-string
                                    (dmenu "Eval:" (lambda () '())))))))
 
 
-(define-key *top-map* (kbd "s-M-h") 
+(define-key *top-map* (kbd "s-M-h")
             (lambda () (resize-client-by *selected* 0 0 -50 0)))
-(define-key *top-map* (kbd "s-M-j") 
+(define-key *top-map* (kbd "s-M-j")
             (lambda () (resize-client-by *selected* 0 0 0 50)))
-(define-key *top-map* (kbd "s-M-k") 
+(define-key *top-map* (kbd "s-M-k")
             (lambda () (resize-client-by *selected* 0 0 0 -50)))
-(define-key *top-map* (kbd "s-M-l") 
+(define-key *top-map* (kbd "s-M-l")
             (lambda () (resize-client-by *selected* 0 0 50 0)))
 
-(define-key *top-map* (kbd "s-S-h") 
+(define-key *top-map* (kbd "s-S-h")
             (lambda () (resize-client-by *selected* -50 0 0 0)))
-(define-key *top-map* (kbd "s-S-j") 
+(define-key *top-map* (kbd "s-S-j")
             (lambda () (resize-client-by *selected* 0 50 0 0)))
-(define-key *top-map* (kbd "s-S-k") 
+(define-key *top-map* (kbd "s-S-k")
             (lambda () (resize-client-by *selected* 0 -50 0 0)))
-(define-key *top-map* (kbd "s-S-l") 
+(define-key *top-map* (kbd "s-S-l")
             (lambda () (resize-client-by *selected* 50 0 0 0)))
+
+(restart-bars)
