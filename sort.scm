659f82cd9c03db1b48776699cd70c1c1f3e896c5
Ramil Farkhshatov <noble_yoda@mail.ru>
2009-07-31 23:09:22 +0400

    Changed sorting algorithm.

--- sort.scm
+++ sort.scm
@@ -1,37 +1,53 @@
+;;;============================================================================
+;;; File: "Sort.scm", Time-stamp: <2008-03-18 15:21:35 feeley>
+;;; Copyright (c) 2006-2008 by Marc Feeley, All Rights Reserved.
+;;;============================================================================
+;;; (sort sequence less?) sorts a sequence (a list or vector) in a
+;;; non-destructive way ordered using the comparison predicate less?.
+;;;
+;;; Sample use:
+;;;
+;;;  (sort (vector 3 1 4 1 5) >) => #(5 4 3 1 1)
+;;;============================================================================
 
-(define (qsort lst test)
-  (define (partition lst pivot k)
-    (let loop ((lst lst)
-               (smaller '())
-               (greater '()))
-      (cond
-       ((null? lst)
-        (k smaller greater))
-       ((test (car lst) pivot)
-        (loop (cdr lst)
-              (cons (car lst) smaller)
-              greater))
-       (else
-        (loop (cdr lst)
-              smaller
-              (cons (car lst) greater))))))
+(define (sort sequence less?)
+  (declare (standard-bindings) (not safe))
+  (define (sort-list lst less?)
+    (define (mergesort lst)
+      (define (merge lst1 lst2)
+        (cond ((not (pair? lst1))
+               lst2)
+              ((not (pair? lst2))
+               lst1)
+              (else
+                (let ((e1 (car lst1)) (e2 (car lst2)))
+                  (if (less? e1 e2)
+                      (cons e1 (merge (cdr lst1) lst2))
+                      (cons e2 (merge lst1 (cdr lst2))))))))
+      (define (split lst)
+        (if (or (not (pair? lst)) (not (pair? (cdr lst))))
+            lst
+            (cons (car lst) (split (cddr lst)))))
+      (if (or (not (pair? lst)) (not (pair? (cdr lst))))
+          lst
+          (let* ((lst1 (mergesort (split lst)))
+                 (lst2 (mergesort (split (cdr lst)))))
+            (merge lst1 lst2))))
+    (mergesort lst))
+  (cond ((not (procedure? less?))
+         (error "procedure expected"))
+        ((or (null? sequence)
+             (pair? sequence))
+         (sort-list sequence less?))
+        ((vector? sequence)
+         (list->vector (sort-list (vector->list sequence) less?)))
+        (else
+          (error "vector or list expected"))))
 
-  (define (qs lst sorted)
-    (if (null? lst)
-        sorted
-        (let ((pivot (car lst))
-              (rest (cdr lst)))
-          (partition rest
-                     pivot
-                     (lambda (smaller greater)
-                       (qs smaller
-                           (cons pivot
-                                 (qs greater sorted))))))))
-
-  (qs lst '()))
+;;;============================================================================
 
 (define (remove-duplicates lst key test)
-  (let ((sorted (qsort lst (lambda (a b) (eq? (key a) (key b))))))
+  (let ((sorted (sort lst (lambda (a b) (< (key a) (key b))))))
     (if (pair? sorted)
         (let loop ((lst (cdr sorted))
                    (prev (car sorted))
