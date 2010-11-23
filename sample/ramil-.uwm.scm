11cdae3e9c77c5185095903d00ccb3700554069a
Ramil Farkhshatov <ramil@gmx.co.uk>
2010-11-24 02:10:57 +0300

    Updated my scripts and cfg.

--- sample/ramil-.uwm.scm
+++ sample/ramil-.uwm.scm
@@ -24,9 +24,9 @@
              (str (string-append (current-view)
                                  " "
                                  (string-current-layout)
-                                 " /"
+                                 " <"
                                  (prev-view)
-                                 "/")))
+                                 ">")))
     (if (pair? tags)
         (loop (cdr tags)
               (if (or (string=? (car tags) (current-view))
