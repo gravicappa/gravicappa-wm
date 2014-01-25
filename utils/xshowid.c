cd6c75e41e530b1c184bfea87b76777c8691a64f
Ramil Farkhshatov <ramil@gmx.co.uk>
2014-01-25 11:14:09 +0400

    Added centered mode. Cleaned font handling.

--- utils/xshowid.c
+++ utils/xshowid.c
@@ -4,18 +4,15 @@
 #include <string.h>
 #include <stdarg.h>
 
-#define NFONTS 2
-const char *fontlist[] = {
-  "-misc-fixed-bold-r-*-*-18-*-*-*-*-*-iso10646-1",
-  "fixed"
-};
+const char *fontstr = "-misc-fixed-bold-r-*-*-18-*-*-*-*-*-iso10646-1";
 
-enum {
+enum conf {
   COLOUR_BG = 0x700000,
   COLOUR_FG = 0xffffff,
   COLOUR_SEL_FG = 0xffffff,
   COLOUR_SEL_BG = 0x000000,
-  BORDER = 4
+  BORDER = 4,
+  CENTER = 0,
 };
 
 struct node {
@@ -33,7 +30,7 @@ struct font {
 };
 
 xcb_connection_t *c;
-struct font fonts[NFONTS];
+struct font font;
 struct node *windows;
 
 void release();
@@ -50,23 +47,20 @@ die(const char *fmt, ...)
 }
 
 int
-strn_width(xcb_gcontext_t gc, int len, const char *str)
+strn_width(int len, const char *str)
 {
 	int i, ch, w = 0;
-	struct font *fn = &fonts[0];
 	for (i = 0; i < len && (ch = str[i]); ++i)
-		w += ((ch > fn->range[0] && ch < fn->range[1]) 
-					? fn->tbl[ch - fn->range[0]].character_width : 0);
+		w += ((ch > font.range[0] && ch < font.range[1])
+					? font.tbl[ch - font.range[0]].character_width : 0);
 	return w;
 }
 
 void
 draw_strn(struct node *node, int len, const char *str, int x, int y)
 {
-	struct font *fn = &fonts[0];
-
-	xcb_change_gc(c, node->gc, XCB_GC_FONT, &fn->fn);
-	y += fn->height - fn->info->font_descent;
+	xcb_change_gc(c, node->gc, XCB_GC_FONT, &font.fn);
+	y += font.height - font.info->font_descent;
 	xcb_image_text_8(c, len, node->win, node->gc, x, y, str);
 }
 
@@ -101,32 +95,25 @@ get_win_info(xcb_window_t win, int r[4], xcb_screen_t **scr)
 }
 
 void
-init_fonts()
+init_font()
 {
-	int h, maxh, i, n = sizeof(fonts) / sizeof(fonts[0]);
 	xcb_query_font_cookie_t req;
 	xcb_query_font_reply_t *info;
 	xcb_void_cookie_t cookie;
-	xcb_font_t font;
+	xcb_font_t fn;
 
-	maxh = -1;
-	for (i = 0; i < n; ++i) {
-		font = xcb_generate_id(c);
-		cookie = xcb_open_font_checked(c, font, strlen(fontlist[i]), fontlist[i]);
-		if (xcb_request_check(c, cookie))
-			die("Could not load font '%s'.\n", fontlist[i]);
-		req = xcb_query_font(c, font);
-		info = xcb_query_font_reply(c, req, 0);
-		fonts[i].fn = font;
-		fonts[i].info = info;
-		fonts[i].tbl = xcb_query_font_char_infos(info);
-		fonts[i].range[0] = info->min_byte1 << 8 | info->min_char_or_byte2;
-		fonts[i].range[1] = info->max_byte1 << 8 | info->max_char_or_byte2;
-		h = info->font_ascent + info->font_descent;
-		maxh = (maxh > h) ? maxh : h;
-	}
-	for (i = 0; i < n; ++i)
-		fonts[i].height = maxh;
+  fn = xcb_generate_id(c);
+  cookie = xcb_open_font_checked(c, fn, strlen(fontstr), fontstr);
+  if (xcb_request_check(c, cookie))
+    die("Could not load font '%s'.\n", fontstr);
+  req = xcb_query_font(c, fn);
+  info = xcb_query_font_reply(c, req, 0);
+  font.fn = fn;
+  font.info = info;
+  font.tbl = xcb_query_font_char_infos(info);
+  font.range[0] = info->min_byte1 << 8 | info->min_char_or_byte2;
+  font.range[1] = info->max_byte1 << 8 | info->max_char_or_byte2;
+  font.height = info->font_ascent + info->font_descent;
 }
 
 int
@@ -147,10 +134,14 @@ mk_num_window(xcb_screen_t *scr, int r[4], int num)
 
   nstr = snprintf(str, sizeof(str), "%d", num);
 	node->win = xcb_generate_id(c);
+  rect.width = strn_width(nstr, str) + BORDER * 2;
+  rect.height = font.height + BORDER * 2;
   rect.x = (r[0] >= 0) ? r[0] : 0;
   rect.y = (r[1] >= 0) ? r[1] : 0;
-  rect.width = 100;
-  rect.height = 100;
+  if (CENTER) {
+    rect.x += (r[2] - rect.width) >> 1;
+    rect.y += (r[3] - rect.height) >> 1;
+  }
 	mask = XCB_CW_BACK_PIXEL | XCB_CW_OVERRIDE_REDIRECT | XCB_CW_EVENT_MASK;
 	val[0] = scr->white_pixel;
 	val[1] = 1;
@@ -166,10 +157,6 @@ mk_num_window(xcb_screen_t *scr, int r[4], int num)
 	val[2] = 0;
 	node->gc = xcb_generate_id(c);
 	xcb_create_gc(c, node->gc, node->win, mask, val);
-  val[0] = strn_width(node->gc, nstr, str) + BORDER * 2;
-  val[1] = fonts[0].height + BORDER * 2;
-  mask = XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT;
-	xcb_configure_window(c, node->win, mask, val);
   val[0] = COLOUR_BG;
   val[1] = 0;
   xcb_change_gc(c, node->gc, XCB_GC_FOREGROUND, val);
@@ -189,16 +176,13 @@ mk_num_window(xcb_screen_t *scr, int r[4], int num)
 void
 release()
 {
-	int i, n = sizeof(fonts) / sizeof(fonts[0]);
   struct node *node, *nodenext;
 
 	xcb_ungrab_keyboard(c, XCB_CURRENT_TIME);
-	for (i = 0; i < n; ++i) {
-		if (fonts[i].info)
-			free(fonts[i].info);
-		if (fonts[i].fn)
-			xcb_close_font(c, fonts[i].fn);
-	}
+  if (font.info)
+    free(font.info);
+  if (font.fn)
+    xcb_close_font(c, font.fn);
   for (node = windows; node; node = nodenext) {
     nodenext = node->next;
     if (node->win)
@@ -237,7 +221,7 @@ main()
   c = xcb_connect(0, 0);
   if (!c)
     die("Cannot open display.\n");
-  init_fonts();
+  init_font();
   while (fgets(buf, sizeof(buf), stdin))
     if (sscanf(buf, "%u", &win) == 1
         && !get_win_info(win, r, &s) && !mk_num_window(s, r, i))
